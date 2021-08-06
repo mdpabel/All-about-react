92a330121e4499a113de7d127a47e01eae813519
mdpabel <mdpabel385@gmail.com>
2021-08-06 21:17:22 +0600

    added filter func

--- 05. React performance/src/components/Local-Card.js
+++ 05. React performance/src/components/Local-Card.js
@@ -1,15 +1,22 @@
 import React from "react";
 import data from "../data";
+import { filter } from "../use-filter";
 import { Card } from "./Card";
 
-const SyncCard = () => {
+const SyncCard = ({ search }) => {
+  const filteredData = React.useMemo(() => filter(data, search), [search]);
+  const items = filteredData.slice(0, 100);
+
   return (
     <div
       style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
     >
-      {data.map(({ thumbnailUrl, title, id }) => (
+      {items.map(({ thumbnailUrl, title, id }) => (
         <Card key={id} img={thumbnailUrl} title={title} />
       ))}
+      {items.length <= 0 ? (
+        <h3>No data found with the keyword : {search}</h3>
+      ) : null}
     </div>
   );
 };
